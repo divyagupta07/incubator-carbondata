436874f619cee8eae6e7b946ee2638605b0df1ec
Divya <divya@knoldus.com>
2017-08-17 16:55:40 +0530

    WIP CarbonOutputFormat

--- hadoop/src/main/java/org/apache/carbondata/hadoop/CarbonRecordWriter.java
+++ hadoop/src/main/java/org/apache/carbondata/hadoop/CarbonRecordWriter.java
@@ -9,14 +9,18 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
 
 public class CarbonRecordWriter<T> extends RecordWriter<Void, T> {
 
+  private final CarbonWriteSupport<T> writeSupport;
+
   public CarbonRecordWriter(CarbonLoadModel loadModel, CarbonWriteSupport<T> writeSupport)
       throws IOException {
-    //checkAndCreateCarbonDataLocation
-    writeSupport.initialize(loadModel, null);
+    //handlesegmentmerging
+    this.writeSupport = writeSupport;
+
   }
 
   @Override public void write(Void key, T value) throws IOException, InterruptedException {
-
+    writeSupport.write(value);
+    //checkblocksizereached
   }
 
   @Override public void close(TaskAttemptContext context) throws IOException, InterruptedException {
